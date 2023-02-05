3c78d83449e958371444a54bfbed9a27f7b8a16d
DanielGCh1 <58349941+DanielGCh1@users.noreply.github.com>
2023-02-05 13:32:17 -0600

    Pasar entre pantallas

---
+++ Assets/ScriptsDaniel/GameHelp.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameHelp : MonoBehaviour
+{
+    // REFERENCIA
+    public void OnBackSceneHowToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay", LoadSceneMode.Single);
+    }
+    public void OnBackSceneFirstWorld()
+    {
+        SceneManager.LoadScene("FirstWorld", LoadSceneMode.Single);
+    }
+
+}
